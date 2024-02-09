d4c5a8bc1f79805502789816cad52022a203d58e
FarahFrgne05 <farah.ferguene@efrei.net>
2024-02-09 15:08:44 +0100

    Step 1 - SafeCalculator

--- java/src/test/java/info/dmerej/SafeCalculatorTest.java
+++ java/src/test/java/info/dmerej/SafeCalculatorTest.java
@@ -2,9 +2,15 @@ package info.dmerej;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SafeCalculatorTest {
   @Test
   void should_not_throw_when_authorized() {
     // TODO: write a test to demonstrate the bug in SafeCalculator.add()
+    SafeCalculator calculator = new SafeCalculator(() -> true);
+    int actual = calculator.add(1, 2);
+    assertEquals(3, actual);
+
   }
 }
